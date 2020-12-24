86ff4f4c3e85cec590154d13037431fea8843c0c
Hugo Carvalho <huguinho66@live.com>
2020-12-24 19:18:29 +0000

    readfile, and prints

--- MiniProjeto.c
+++ MiniProjeto.c
@@ -1,156 +1,134 @@
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
-#define MAX 25
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define MAX 25
+#define EMPTY '_'
+#define ARMED '.'
+#define DISARMED '*'
 
 void menu();
 void choices();
-void readFile(filename);
-int grelha();
+void readFile(char filename[200], char grid[][MAX]);
 
-int main(){
-	menu();
-	choices();
-	
-}	
-
-/// Imprime o menu
-/// Verifica a escolha do utilizador 
-void menu(){
-	puts("+-----------------------------------------------------");
-	puts("read <filename>     - read input file");
-	puts("show                - show the mine map");
-	puts("trigger <x> <y>     - trigger mine at <x> <y>");
-	puts("plant <x> <y>       - place armed mine at <x> <y>");
-	puts("export <filename>   - save file with current map");
-	puts("quit                - exit program");
-	puts("+-----------------------------------------------------");
+int main()
+{
+    menu();
+    choices();
 }
 
-void choices(){
+/// Imprime o menu
+void menu()
+{
+    puts("+-----------------------------------------------------");
+    puts("read <filename>     - read input file");
+    puts("show                - show the mine map");
+    puts("trigger <x> <y>     - trigger mine at <x> <y>");
+    puts("plant <x> <y>       - place armed mine at <x> <y>");
+    puts("export <filename>   - save file with current map");
+    puts("quit                - exit program");
+    puts("+-----------------------------------------------------");
+}
 
-	char choice[20];
-	int i = 1;
-	char filename[200];
-	
-	do
-	{
-		printf("\n>");
-		// lê a escolha e o path do ficheiro
-		///scanf("%s%s",choice, filename );
-		if(scanf("\n%s",&choice)){
-			// compares the input with the menu options
-			if(strcmp(choice,"read") == 0)
-			{
-				if(scanf("%s", &filename)){
-					readFile(filename);
+void choices()
+{
+    int i = 1;
+    char choice[20];
+    char filename[200];
+    char game[MAX][MAX];
 
-				}
-			
-				
-				
-			}
-			else if(strcmp(choice,"show") == 0)
-			{
-				//printmap()
-			}
-			else if(strcmp(choice,"trigger") == 0)
-			{
-			}
-			else if(strcmp(choice,"plant") == 0)
-			{
-			}
-			else if(strcmp(choice,"export") == 0)
-			{
+    do
+    {
+        printf("\n>");
+        // lê a escolha e o path do ficheiro
+        ///scanf("%s",choice);
+        if (scanf("\n%s", &choice))
+        {
+            // compares the input with the menu options
+            if (strcmp(choice, "read") == 0)
+            {
+                if (scanf("%s", &filename))
+                {
+                    readFile(filename, game);
 
-			}
-			else if(strcmp(choice,"quit") == 0)
-			{
-				exit(1);
-			}
-			else if(strcmp(choice,"sos") == 0)
-			{
-				menu();
-			}
-			else
-			{
-				printf("Inputs errados.\n");
-			}
+                    for (int i = 0; i < MAX; i++)
+                    {
+                        for (int j = 0; j < MAX; j++)
+                        {
+                            printf("%c", game[i][j]);
+                        }
+                    }
+                }
+            }
+            else if (strcmp(choice, "show") == 0)
+            {
+                //printmap();
+            }
+            else if (strcmp(choice, "trigger") == 0)
+            {
+            }
+            else if (strcmp(choice, "plant") == 0)
+            {
+            }
+            else if (strcmp(choice, "export") == 0)
+            {
+            }
+            else if (strcmp(choice, "quit") == 0)
+            {
+                exit(1);
+            }
+            else if (strcmp(choice, "sos") == 0)
+            {
+                menu();
+            }
+            else
+            {
+                printf("Inputs errados.\n");
+            }
+        }
 
-		}
-		
-		
-			
-		
-			
-	} while (i != 0);
+    } while (i != 0);
 }
 
 // Lê o ficheiro
-void readFile(char filename[200]){
-	int *chx;
-	int *chy;
-	int x;
-	
-	//Apontador para o ficheiro 
-	FILE *file;
+void readFile(char filename[200], char grid[][MAX])
+{
+    int chx;
+    int chy;
+    int x;
 
-	// nome do ficheiro
-	// localização do ficheiro
+    //Apontador para o ficheiro
+    FILE *file;
 
-	//Abre o ficheiro
-	file = fopen(filename,"r");
+    // nome do ficheiro
+    // localização do ficheiro
 
-	// Verifica se o ficheiro é valido
+    //Abre o ficheiro
+    file = fopen(filename, "r");
 
-	if(filename == NULL){
-		printf("Impossivel abrir o ficheiro %s", filename);
-	}
-	else
-	{
-		x = grelha();
-		//Imprime tudo que está no ficheiro
-		while (fscanf(file,"%d %d",&chx, &chy)!= EOF)
-		{ 
-			printf("x%dy%d\n", chx, chy);
+    // Verifica se o ficheiro é valido
 
-			x [chx][chy] = 1;
-			
+    if (filename == NULL)
+    {
+        printf("Impossivel abrir o ficheiro %s", filename);
+    }
+    else
+    {
+        for (int i = 0; i < MAX; i++)
+        {
+            for (int j = 0; j < MAX; j++)
+            {
+                grid[i][j] = EMPTY;
+            }
+        }
 
+        //Imprime tudo que está no ficheiro
+        while (fscanf(file, "%d %d", &chx, &chy) != EOF)
+        {
+            printf("%d %d\n", chx, chy);
 
-		}
-		
-		fclose(file);
-		
-	}
-	
-	return 0;
-
+            grid[chx][chy] = ARMED;
+        }
+        fclose(file);
+    }
 }
-
-
-// faz a grelha
-int grelha(){
-
-	int me[MAX][MAX];
-
-	
-	int i = 0;
-	int j = 0;
-
-	for (i = 0; i < 25; i++)
-	{
-		for(j = 0; j < 25; j++){
-			me[i][j] = 0;
-			printf("%d", me[i][j]);
-			 
-		}
-	}
-
-
-
-	return me;
-}
-
-
